6e1ff895656e88cdd858432b6976a7358319de24
Jeffrey <jeff83636tx@gmail.com>
2022-12-09 15:28:27 -0600

    feat: test

--- stage6/delete.C
+++ stage6/delete.C
@@ -1,7 +1,6 @@
 #include "catalog.h"
 #include "query.h"
 
-
 /*
  * Deletes records from a specified relation.
  *
@@ -10,17 +9,64 @@
  * 	an error code otherwise
  */
 
-const Status QU_Delete(const string & relation, 
-		       const string & attrName, 
-		       const Operator op,
-		       const Datatype type, 
-		       const char *attrValue)
+const Status QU_Delete(const string &relation,
+					   const string &attrName,
+					   const Operator op,
+					   const Datatype type,
+					   const char *attrValue)
 {
-// part 6
-return OK;
+	Status status;
 
+	HeapFileScan *hfs = new HeapFileScan(relation, status);
+	if (status != OK)
+		return status;
+	// Get attribute info
+	AttrDesc attrInfo;
+	status = attrCat->getInfo(relation, attrName, attrInfo);
+	if (status != OK)
+		return status;
 
+	// Get filter value
+	const char *filter;
+	if (type == STRING)
+	{
+		filter = attrValue;
+	}
+	else if (type == INTEGER)
+	{
+		int temp = atoi(attrValue);
+		filter = (char *)(&temp);
+	}
+	else if (type == FLOAT)
+	{
+		float temp = atof(attrValue);
+		filter = (char *)(&temp);
+	}
+	else
+	{
+		// No filtering needed
+		filter = NULL;
+	}
 
+	// Scan through the relation
+	status = hfs->startScan(attrInfo.attrOffset, attrInfo.attrLen, type, filter, op);
+	if (status != OK)
+		return status;
+	RID rid;
+	Record rec;
+	while (hfs->scanNext(rid) == OK)
+	{
+		status = hfs->getRecord(rec);
+		if (status != OK)
+			return status;
+
+		status = hfs->deleteRecord();
+		if (status != OK)
+			return status;
+	}
+	status = hfs->endScan();
+	if (status != OK)
+		return status;
+	delete hfs;
+	return OK;
 }
-
-
